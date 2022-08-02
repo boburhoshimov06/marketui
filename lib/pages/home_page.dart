9bc115b5953cf9b59124513b913acd08e3cd8514
boburhoshimov06 <boburhoshimov06@gmail.com>
2022-08-02 15:15:27 +0500

    add images

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,8 +13,29 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Page'),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'Cars',
+          style: TextStyle(color: Colors.black, fontSize: 30),
+        ),
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.notifications_none),
+            color: Colors.black,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart),
+            color: Colors.black,
+          ),
+        ],
+      ),
+      body: Container(
+        color: Colors.green,
       ),
     );
   }
